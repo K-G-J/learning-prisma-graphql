890695b5c88eabedb5f4781610d62cfd960d3d74
Kate Johnson <johnson.kat.g@gmail.com>
2023-03-10 18:54:14 -0500

    part 1 progress

--- data/links.ts
+++ data/links.ts
@@ -1,35 +1,36 @@
 export const links = [
   {
-    category: "Open Source",
-    description: "Fullstack React framework",
+    category: 'Open Source',
+    description: 'Fullstack React framework',
     id: 1,
-    imageUrl: "https://github.com/vercel.png",
-    title: "Next.js",
-    url: "https://nextjs.org",
+    imageUrl: 'https://nextjs.org/static/twitter-cards/home.jpg',
+    title: 'Next.js',
+    url: 'https://nextjs.org'
   },
   {
-    category: "Open Source",
-    description: "Next Generation ORM for TypeScript and JavaScript",
+    category: 'Open Source',
+    description: 'Next Generation ORM for TypeScript and JavaScript',
     id: 2,
-    imageUrl: "https://github.com/prisma.png",
+    imageUrl: 'https://www.prisma.io/images/og-image.png',
 
-    title: "Prisma",
-    url: "https://prisma.io",
+    title: 'Prisma',
+    url: 'https://prisma.io'
   },
   {
-    category: "Open Source",
-    description: "Utility-fist css framework",
+    category: 'Open Source',
+    description: 'Utility-fist css framework',
     id: 3,
-    imageUrl: "https://github.com/tailwindlabs.png",
-    title: "TailwindCSS",
-    url: "https://tailwindcss.com",
+    imageUrl:
+      'https://tailwindcss.com/_next/static/media/twitter-large-card.85c0ff9e455da585949ff0aa50981857.jpg',
+    title: 'TailwindCSS',
+    url: 'https://tailwindcss.com'
   },
   {
-    category: "Open Source",
-    description: "GraphQL implementation ",
+    category: 'Open Source',
+    description: 'GraphQL implementation ',
     id: 4,
-    imageUrl: "https://www.apollographql.com/apollo-home.jpg",
-    title: "Apollo GraphQL",
-    url: "https://apollographql.com",
-  },
+    imageUrl: 'https://www.apollographql.com/apollo-home.jpg',
+    title: 'Apollo GraphQL',
+    url: 'https://apollographql.com'
+  }
 ];
